7c7f5fa587743f3e4cfd56f5af1ac26a812f4458
Jessica Vega <jvega@tiempodevelopment.com>
2018-05-29 13:02:19 -0500

    test message

--- Test Suites/TestSuiteColletion.ts
+++ Test Suites/TestSuiteColletion.ts
@@ -4,6 +4,7 @@
    <name>TestSuiteColletion</name>
    <tag></tag>
    <isRerun>false</isRerun>
+   <lastRun>2018-05-29T12:44:04</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
@@ -11,15 +12,15 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>833234b7-0828-4ca5-9a7e-1c3dddce66c2</testSuiteGuid>
    <testCaseLink>
-      <guid>670985d8-616a-47bf-9c9b-0ba1322d4163</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TestsByKim/Test2ByKim</testCaseId>
-   </testCaseLink>
-   <testCaseLink>
       <guid>58680352-77fa-4a14-b13a-0ded742ae02d</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/TestsByTom/Test1ByTom</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>670985d8-616a-47bf-9c9b-0ba1322d4163</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestsByKim/Test2ByKim</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
