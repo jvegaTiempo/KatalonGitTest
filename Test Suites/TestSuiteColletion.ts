e52b62913d38c71c5379d5ec26772cd4db0291ac
Jessica Vega <jvega@tiempodevelopment.com>
2018-05-29 12:21:01 -0500

    Test2ByKim and TestSuitecollection

---
+++ Test Suites/TestSuiteColletion.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Test1 and Test2</description>
+   <name>TestSuiteColletion</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>833234b7-0828-4ca5-9a7e-1c3dddce66c2</testSuiteGuid>
+   <testCaseLink>
+      <guid>670985d8-616a-47bf-9c9b-0ba1322d4163</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestsByKim/Test2ByKim</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>58680352-77fa-4a14-b13a-0ded742ae02d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestsByTom/Test1ByTom</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
