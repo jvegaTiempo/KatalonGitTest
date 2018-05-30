d2916c5cd4a517b5e5210d9c704404d0ffbefbdd
Jessica Vega <jvega@tiempodevelopment.com>
2018-05-30 11:15:32 -0500

    Uploading Changes to GitHub

--- Test Suites/TestSuiteColletion.ts
+++ Test Suites/TestSuiteColletion.ts
@@ -4,7 +4,7 @@
    <name>TestSuiteColletion</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-05-30T11:12:39</lastRun>
+   <lastRun>2018-05-30T11:14:55</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
